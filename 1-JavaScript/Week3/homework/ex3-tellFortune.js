df0faf2e9da35b187a16eac5f1a6d09272c97b36
Lydia Samir <L-Samir@hotmail.com>
2021-07-13 17:16:36 +0200

    added the answers of the homework exercises

--- 1-JavaScript/Week3/homework/ex3-tellFortune.js
+++ 1-JavaScript/Week3/homework/ex3-tellFortune.js
@@ -3,55 +3,53 @@
 Why pay a fortune teller when you can just program your fortune yourself?
 
 1. Create four arrays, `numKids`, `partnerNames`, `locations` and `jobTitles`. 
-   Give each array five random values that have to do with the name of 
-   the variable.
+  Give each array five random values that have to do with the name of 
+  the variable.
 
 2. Complete the function `selectRandomly`. This function should take an array 
-   as a parameter and return a randomly selected element as its return value.
+  as a parameter and return a randomly selected element as its return value.
 
 3. Complete the function named `tellFortune` as follows:
 
-   - It should take four arguments (in the order listed): 
+  - It should take four arguments (in the order listed): 
      * number of children (`number`), 
      * partner's name (`string`), 
      * geographic location (`string`) and 
      * job title (`string`).
-   - It should use the `selectRandomly` function to randomly select values from 
-     the arrays.
-   - It should return a string: "You will be a `jobTitle` in `location`, 
+  - It should use the `selectRandomly` function to randomly select values from 
+    the arrays.
+  - It should return a string: "You will be a `jobTitle` in `location`, 
     married to `partnerName` with `numKids` kids."
 
 4. Call the function three times, passing the arrays as arguments. Use `
-   console.log` to display the results.
+  console.log` to display the results.
 
 Note: The DRY is put into practice here: instead of repeating the code to 
 randomly select array elements four times inside the `tellFortune` function 
 body, this code is now written once only in a separated function.
 -----------------------------------------------------------------------------*/
-const numKids = [
-  // TODO add elements here
-];
+const numKids = [2, 3, 1, 3, 5];
 
-const partnerNames = [
-  // TODO add elements here
-];
+const partnerNames = ['Amalia', 'Hany', 'Gloria', 'George', 'Tom'];
 
-const locations = [
-  // TODO add elements here
-];
+const locations = ['Paris', 'Cairo', 'Rome', 'Amsterdam', 'Bali'];
 
-const jobTitles = [
-  // TODO add elements here
-];
+const jobTitles = ['Web Developer', 'Dancer', 'Actor/Actress', 'Driver', 'CEO'];
 
 // This function should take an array as its parameter and return
 // a randomly selected element as its return value.
-function selectRandomly(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function selectRandomly(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function tellFortune(/* add parameter(s) here */) {
-  // TODO complete this function
+function tellFortune(param1, param2, param3, param4) {
+  return `You will have ${selectRandomly(
+    param1
+  )} Kids, you partner's name is ${selectRandomly(
+    param2
+  )}, you will live in ${selectRandomly(
+    param3
+  )}, and your job will be ${selectRandomly(param4)}`;
 }
 
 console.log(tellFortune(numKids, partnerNames, locations, jobTitles));
--- 1-JavaScript/Week3/homework/ex5-shoppingCartPure.js
+++ 1-JavaScript/Week3/homework/ex5-shoppingCartPure.js
@@ -4,18 +4,26 @@ In the current exercise we will rewrite the `addToShoppingCart` function to make
 it pure. Do the following:
 
 1. Complete the parameter list of `addToShopping()`. As a first parameter it 
-   should accept a shopping cart array and as a second parameter it should 
-   accept a grocery item to be added.
+  should accept a shopping cart array and as a second parameter it should 
+  accept a grocery item to be added.
 2. The function should return a new shopping cart array, following the same rule 
-   as in the previous exercise: it should contain a maximum of three items.
+  as in the previous exercise: it should contain a maximum of three items.
 3. The shopping cart passed as an argument should not be mutated.
 4. When constructing the new shopping cart array you should make use of the ES5 
-   spread syntax.
+  spread syntax.
 5. Confirm that you function passes the provided unit tests.
 ------------------------------------------------------------------------------*/
 // ! Function under test
-function addToShoppingCart(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+
+function addToShoppingCart(arr, x) {
+  const newArr = [...arr];
+  do {
+    newArr.push(x);
+  } while (newArr.length < 3);
+  if (newArr.length > 3) {
+    newArr.shift();
+  }
+  return newArr;
 }
 
 // ! Test functions (plain vanilla JavaScript)
