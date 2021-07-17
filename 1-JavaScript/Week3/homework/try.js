e8bc9fade80d185be479bd1a443962205642664d
Fikret Yilmaz <yilmz.fikret@gmail.com>
2021-07-17 13:14:47 +0200

    Save first question w4-JS in w3-js branch

--- 1-JavaScript/Week3/homework/try.js
+++ 1-JavaScript/Week3/homework/try.js
@@ -30,3 +30,13 @@ function calculateTotalPrice(items) {
 }
 
 calculateTotalPrice(cartForParty);
+
+const numbers = [2, 4, 6, 8, 10];
+
+function addTwo(number) {
+  return number + 2;
+}
+
+const numbersPlusTwo = numbers.map(addTwo);
+
+console.log(numbersPlusTwo);
